d2c590e2ff74547941418d283229ac28825e34c5
t-pirinen <toimi.i.pirinen@gmail.com>
2025-05-10 21:34:20 +0300

    added .gitignore

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -17,15 +17,18 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*newstr;
-	size_t	newlen;
+	char	*iter;
 
 	if (!s1 || !s2)
 		return (NULL);
-	newlen = ft_strlen(s1) + ft_strlen(s2);
-	newstr = malloc(newlen + 1);
-	if (newstr == NULL)
+	newstr = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!newstr)
 		return (NULL);
-	ft_strlcpy(newstr, s1, ft_strlen(s1) + 1);
-	ft_strlcat(newstr, s2, newlen + 1);
+	iter = newstr;
+	while (*s1)
+		*iter++ = *s1++;
+	while (*s2)
+		*iter++ = *s2++;
+	*iter = '\0';
 	return (newstr);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,111 +12,22 @@
 
 #include "libft.h"
 
-static int	ft_isinset(char const *set, char c);
-static int	ft_startlen(char const *s1, char const *set);
-static int	ft_endlen(char const *s1, char const *set);
-static void	ft_cpystr(char *trim, char const *s1, size_t startl, size_t triml);
-
 /*	Allocates memory (using malloc(3)) and returns a copy of ’s1’ with
 	characters from ’set’ removed from the beginning and the end.			*/
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*trim;
-	size_t	i;
-	size_t	triml;
-	size_t	startl;
-	size_t	endl;
+	size_t	start;
+	size_t	end;
+	size_t	len;
 
 	if (!s1 || !set)
 		return (NULL);
-	i = 0;
-	startl = ft_startlen(s1, set);
-	if (startl == ft_strlen(s1))
-	{
-		trim = malloc(1);
-		trim[i] = '\0';
-		return (trim);
-	}
-	endl = ft_endlen(s1, set);
-	triml = ft_strlen(s1) - (startl + endl);
-	trim = malloc(triml + 1);
-	if (trim == NULL)
-		return (NULL);
-	ft_cpystr(trim, s1, startl, triml);
-	return (trim);
-}
-
-/*	Checks if the character 'c' is found in the string 'set'.				*/
-static int	ft_isinset(char const *set, char c)
-{
-	while (*set != '\0')
-	{
-		if (*set == c)
-			return (1);
-		set++;
-	}
-	return (0);
-}
-
-/*	Returns the length of the beginning part of the string 's1'
-	consisting only of the characters in 'set'.								*/
-static int	ft_startlen(char const *s1, char const *set)
-{
-	int		isinset;
-	size_t	i;
-	size_t	len;
-
-	i = 0;
-	len = 0;
-	isinset = ft_isinset(set, s1[0]);
-	if (isinset == 0)
-		return (0);
-	i++;
-	while (isinset != 0)
-	{
-		isinset = ft_isinset(set, s1[i]);
-		len++;
-		i++;
-	}
-	return (len);
-}
-
-/*	Returns the length of the end part of the string 's1'
-	consisting only of the characters in 'set'.								*/
-static int	ft_endlen(char const *s1, char const *set)
-{
-	int		isinset;
-	size_t	i;
-	size_t	len;
-	size_t	slen;
-
-	len = 0;
-	slen = ft_strlen(s1);
-	i = slen - 1;
-	isinset = ft_isinset(set, s1[i]);
-	if (isinset == 0)
-		return (0);
-	i--;
-	while (isinset != 0)
-	{
-		isinset = ft_isinset(set, s1[i]);
-		len++;
-		i--;
-	}
-	return (len);
-}
-
-/*	Assigns the string 's1' skipping the beginning consisting of characters
-	in set and ending before the end part with characters from 'set'.		*/
-static void	ft_cpystr(char *trim, char const *s1, size_t startl, size_t triml)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < triml)
-	{
-		trim[i] = s1[startl + i];
-		i++;
-	}
-	trim[i] = '\0';
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	len = end - start;
+	return (ft_substr(s1, start, len));
 }
